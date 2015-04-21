e19f37a8743f0ad9c5ffeb8d74a6d861cc3b6f01
perborgen <perhb@hotmail.com>
2015-04-21 18:18:21 +0100

    stuff

---
+++ src/js/components/PuzzleApp.js
@@ -0,0 +1,78 @@
+var React           = require("react");
+var Board           = require("./sections/Board");
+var PuzzlePieceMenu = require("./sections/PuzzlePieceMenu");
+
+var PuzzleApp = React.createClass({
+    getInitialState: function(){
+			return {
+				position: {
+					x : 0,
+					y  : 0
+				}
+			};
+		},
+
+		update: function(e){
+			this.setState({
+				position: {
+					x : 0,
+					y : 0
+				}
+			});
+		},
+
+		onDragStart: function(e){
+			console.log('onDragStart triggered');
+			this.setState({
+				position: {
+					x: e.pageX - 88,
+					y: e.pageY - 88
+				}
+			});
+		},
+
+		onDragEnd: function(e){
+			console.log('onDragEnd triggered');
+			this.setState({
+				position: {
+					x: e.pageX - 88,
+					y: e.pageY - 88
+				}
+			});
+		},
+
+		onDrag: function(e){
+			this.setState({
+				position: {
+					x: e.pageX - 88,
+					y: e.pageY - 88
+				}
+			});
+		},
+
+		onClick: function(e){
+			console.log('onClick');
+			console.log(e.pageX);
+			this.setState({
+				position: {
+					x: e.pageX - 88,
+					y: e.pageY - 88
+				}
+			});
+		},
+    render: function() {
+        return (
+            <div>
+                <div className="row">
+                    <Board position={this.state.position} />
+                </div>
+                <div className="row">
+                    <PuzzlePieceMenu position={this.state.position} />
+                </div>
+            </div>
+        );
+    }
+    
+});
+
+module.exports = PuzzleApp;
---
+++ src/js/components/sections/PuzzlePieceMenu.js
@@ -0,0 +1,32 @@
+var React = require('react');
+var Board = require('./Board.js');
+
+var PuzzlePieceMenu = React.createClass({
+
+    render: function(){
+		console.log("this.props:", this.props);
+		var position = this.props.position;
+		console.log('position props: ', position);
+
+		var imgStyle = {
+			"transform": "translate(" + this.props.position.x + "px, " + this.props.position.y +"px) translateZ(0) scale(1.0, 1.0)",
+			"webkit-transform": "translate(" + this.props.position.x + "px, " + this.props.position.y +"px)",
+			position: 'relative',
+			opacity: 1
+		};
+
+
+		return(
+			<div style={imgStyle} >
+			<img 	onClick={this.props.clickHandler} 
+					onDrag={this.props.onDragHandler} 
+					onDragStart={this.props.dragHandler} 
+					onDragEnd={this.props.onDragEndHandler}
+					draggable={true}
+					src="./assets/img/troll.png" />
+			</div>
+		);
+    }
+});
+
+module.exports = PuzzlePieceMenu;
