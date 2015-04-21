7579ea9cecefd9e2b3472246ce9fe6c49e27062f
Anita Amini <baadge29@yahoo.co.uk>
2015-04-21 16:21:17 +0100

    component rearrangement

--- src/js/components/PuzzleApp.js
+++ src/js/components/PuzzleApp.js
@@ -2,106 +2,21 @@ var React           = require("react");
 var Board           = require("./sections/Board");
 var PuzzlePieceMenu = require("./sections/PuzzlePieceMenu");
 
-React.initializeTouchEvents(true);
-
 var PuzzleApp = React.createClass({
     
-		getInitialState: function(){
-			return {
-				position: {
-					x : 0,
-					y  : 0
-				}
-			}
-		},
-
-		update: function(e){
-			this.setState({
-				position: {
-					x : 0,
-					y : 0
-				}
-			});
-		},
-
-		onDragStart: function(e){
-			console.log('onDragStart triggered');
-			this.setState({
-				position: {
-					x: e.pageX - 88,
-					y: e.pageY - 88
-				}
-			});
-		},
-
-		onDragEnd: function(e){
-			console.log('onDragEnd triggered');
-			this.setState({
-				position: {
-					x: e.pageX - 88,
-					y: e.pageY - 88
-				}
-			});
-		},
-
-		onDrag: function(e){
-			this.setState({
-				position: {
-					x: e.pageX - 88,
-					y: e.pageY - 88
-				}
-			});
-		},
-
-		onClick: function(e){
-			console.log('onClick');
-			console.log(e.pageX);
-			this.setState({
-				position: {
-					x: e.pageX - 88,
-					y: e.pageY - 88
-				}
-			});
-		},
-		render: function(){
-			return (
-				<div>Hello there
-					<PuzzleBit position={this.state.position} clickHandler={this.onClick} onDragHandler={this.onDrag} dragHandler={this.onDragStart} onDragEndHandler={this.onDragEnd} />
-				</div>
-				);
-		}
-	});
-
-	var PuzzleBit = React.createClass({
-
-		render: function(){
-		console.log(this.props);
-		var position = this.props.position;
-		console.log('position props: ', position);
-
-		var imgStyle = {
-			"transform": "translate(" + this.props.position.x + "px, " + this.props.position.y +"px) translateZ(0) scale(1.0, 1.0)",
-			"webkit-transform": "translate(" + this.props.position.x + "px, " + this.props.position.y +"px)",
-			position: 'relative',
-			opacity: 1
-		};
-
-
-		return(
-			<div style={imgStyle} >
-			<img 	onClick={this.props.clickHandler} 
-					onDrag={this.props.onDragHandler} 
-					onDragStart={this.props.dragHandler} 
-					onDragEnd={this.props.onDragEndHandler}
-					draggable={true}
-					src="./assets/img/troll.png" />
-			</div>
-		);
-		}
-	});
-
+    render: function() {
+        return (
+            <div>
+                <div className="row">
+                    <Board />
+                </div>
+                <div className="row">
+                    <PuzzlePieceMenu />
+                </div>
+            </div>
+        );
+    }
     
 });
-
 
 module.exports = PuzzleApp;
--- src/js/components/sections/Board.js
+++ src/js/components/sections/Board.js
@@ -1,1 +1,75 @@
 var React = require("react");
+
+
+React.initializeTouchEvents(true);
+
+var PuzzleApp = React.createClass({
+    
+		getInitialState: function(){
+			return {
+				position: {
+					x : 0,
+					y  : 0
+				}
+			};
+		},
+
+		update: function(e){
+			this.setState({
+				position: {
+					x : 0,
+					y : 0
+				}
+			});
+		},
+
+		onDragStart: function(e){
+			console.log('onDragStart triggered');
+			this.setState({
+				position: {
+					x: e.pageX - 88,
+					y: e.pageY - 88
+				}
+			});
+		},
+
+		onDragEnd: function(e){
+			console.log('onDragEnd triggered');
+			this.setState({
+				position: {
+					x: e.pageX - 88,
+					y: e.pageY - 88
+				}
+			});
+		},
+
+		onDrag: function(e){
+			this.setState({
+				position: {
+					x: e.pageX - 88,
+					y: e.pageY - 88
+				}
+			});
+		},
+
+		onClick: function(e){
+			console.log('onClick');
+			console.log(e.pageX);
+			this.setState({
+				position: {
+					x: e.pageX - 88,
+					y: e.pageY - 88
+				}
+			});
+		},
+		render: function(){
+			return (
+				<div>Hello there
+					<PuzzleBit position={this.state.position} clickHandler={this.onClick} onDragHandler={this.onDrag} dragHandler={this.onDragStart} onDragEndHandler={this.onDragEnd} />
+				</div>
+				);
+		}
+	});
+
+
+module.exports = Board;
