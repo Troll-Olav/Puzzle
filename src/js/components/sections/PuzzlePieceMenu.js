758ca1d1364f6dbf1bebf4170ddb16b30a6ec465
perborgen <perhb@hotmail.com>
2015-04-22 14:41:28 +0100

    changes

--- src/js/components/sections/PuzzlePieceMenu.js
+++ src/js/components/sections/PuzzlePieceMenu.js
@@ -16,17 +16,22 @@ componentDidMount: function(){
 	    	console.log('y: ', this.y)
 	    },
 	    onDragEnd: function(e){
-	    	var x = this.x;
-	    	var y = this.y;
+	    	console.log('done');
 	    	var target = e.target;
 	    	var x_goal = target.getAttribute('x');
 	    	var y_goal = target.getAttribute('y');
-	    	var x_diff = Math.abs(x_goal - x);
-	    	var y_diff = Math.abs(y_goal - y);
+	    	var offset = $(target).offset();
+	    	console.log('offset.left: ', offset.left);
+	    	console.log('offset.top: ', offset.top);
+	    	var x_diff = Math.abs(x_goal - offset.left);
+	    	var y_diff = Math.abs(y_goal - offset.top);
+	    	console.log('x_diff: ', x_diff);
+	    	console.log('y_diff: ', y_diff);
+
 	    	if (x_diff < 100 && y_diff < 100) {
 	    		console.log('hit target!');
-	 	    	TweenLite.to(target, 0.5, {x: x_goal,y: y_goal, onComplete: function(){
-	 	    		console.log('completed');
+	 	    	TweenLite.to(target, 0.5, { x: x_goal, y: y_goal, onComplete: function(){
+
 	 	    	}})
 	    	}
 	    }
@@ -36,26 +41,15 @@ componentDidMount: function(){
 	},
 
     render: function(){
-		console.log("this.props:", this.props);
-		var position = this.props.position;
-		console.log('position props: ', position);
-
-		var imgStyle = {
-			"transform": "translate(" + this.props.position.x + "px, " + this.props.position.y +"px) translateZ(0) scale(1.0, 1.0)",
-			"webkit-transform": "translate(" + this.props.position.x + "px, " + this.props.position.y +"px)",
-			position: 'relative',
-			opacity: 1
-		};
 
 		return(
-
-			<div className="row" style={imgStyle} >
+			<div className="row" >
                 <div className="col-md-12">
-                    <img id="troll" x="200" y="200" className="troll" src="./assets/img/Puzzle_A.png" />
-                    <img id="troll" x="200" y="200" className="troll" src="./assets/img/Puzzle_B.png" />
-                    <img id="troll" x="200" y="200" className="troll" src="./assets/img/Puzzle_C.png" />
-                    <img id="troll" x="200" y="200" className="troll" src="./assets/img/Puzzle_D.png" />
-                    <img id="troll" x="200" y="200" className="troll" src="./assets/img/Puzzle_E.png" />
+                    <img  x="200" y="200" className="troll" src="./assets/img/Puzzle_A.png" />
+                    <img  x="200" y="200" className="troll" src="./assets/img/Puzzle_B.png" />
+                    <img  x="200" y="200" className="troll" src="./assets/img/Puzzle_C.png" />
+                    <img  x="200" y="200" className="troll" src="./assets/img/Puzzle_D.png" />
+                    <img  x="-200" y="-200" className="troll" src="./assets/img/Puzzle_E.png" />
                 </div>
 			</div>
 		);
